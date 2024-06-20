c4116bcc1c26cdcd1b6ab9a12caa376ec3a541e3
Kimba SABI N'GOYE <60729256+kimbasabingoye@users.noreply.github.com>
2024-06-20 21:22:00 +0000

    Sync LeetCode submission Runtime - 1544 ms (29.39%), Memory - 0B (100.00%)

--- 1153-product-sales-analysis-i/solution.sql
+++ 1153-product-sales-analysis-i/solution.sql
@@ -1,3 +1,2 @@
-# Write your MySQL query statement below
-select p.product_name, s.year, s.price from sales as s, product as p where s.product_id = p.product_id;
-
+-- Write your PostgreSQL query statement below
+select p.product_name, s.year, s.price from product p join sales s on p.product_id = s.product_id;
