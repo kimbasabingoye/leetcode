6031d2bcb354ff48858ba644feefcd32a78ec5fd
Kimba SABI N'GOYE <60729256+kimbasabingoye@users.noreply.github.com>
2024-06-22 05:14:52 +0000

    Sync LeetCode submission Runtime - 476 ms (89.73%), Memory - 0B (100.00%)

--- 1724-customer-who-visited-but-did-not-make-any-transactions/solution.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/solution.sql
@@ -1,3 +1,2 @@
-# Write your MySQL query statement below
-
-select v.customer_id, count(*) as count_no_trans from visits as v where v.visit_id not in (select visit_id from transactions) group by v.customer_id;
+-- Write your PostgreSQL query statement below
+SELECT customer_id, count(*) as count_no_trans from visits v where v.visit_id not in (select visit_id from transactions) GROUP BY customer_id
