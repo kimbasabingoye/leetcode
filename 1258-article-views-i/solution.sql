6874b2a2d07d7288b8787e359c5f113214e8c019
Kimba SABI N'GOYE <60729256+kimbasabingoye@users.noreply.github.com>
2024-07-13 20:40:46 +0000

    Sync LeetCode submission Runtime - 361 ms (78.35%), Memory - 0B (100.00%)

--- 1258-article-views-i/solution.sql
+++ 1258-article-views-i/solution.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
-select distinct(author_id) as id from views where author_id = viewer_id order by id asc;
+-- Write your PostgreSQL query statement below
+SELECT DISTINCT author_id as id from views WHERE author_id = viewer_id ORDER BY id
