9d655ca20126d07e5422dcc15cf35838b3f32ee5
Kimba SABI N'GOYE <60729256+kimbasabingoye@users.noreply.github.com>
2024-07-13 19:17:45 +0000

    Sync LeetCode submission Runtime - 242 ms (69.48%), Memory - 0B (100.00%)

--- 1908-recyclable-and-low-fat-products/solution.sql
+++ 1908-recyclable-and-low-fat-products/solution.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
-SELECT product_id from Products WHERE low_fats='Y' and recyclable='Y';
+-- Write your PostgreSQL query statement below
+SELECT product_id FROM products WHERE low_fats = 'Y' AND recyclable = 'Y'
