bac217342ef726d995e3fa745163f23865938755
Kimba SABI N'GOYE <60729256+kimbasabingoye@users.noreply.github.com>
2024-07-23 06:45:40 +0000

    Sync LeetCode submission Runtime - 2276 ms (5.11%), Memory - 0B (100.00%)

--- 0607-sales-person/solution.sql
+++ 0607-sales-person/solution.sql
@@ -1,8 +1,13 @@
--- Write your PostgreSQL query statement below
-SELECT  name FROM salesperson s
-WHERE s.sales_id NOT IN
-    (SELECT sales_id
-    FROM orders o
-    JOIN company c
-    ON o.com_id = c.com_id
-    WHERE c.name = 'RED')
+# Write your MySQL query statement below
+
+SELECT s.name
+FROM orders o
+RIGHT JOIN salesperson s
+ON o.sales_id = s.sales_id
+GROUP BY s.sales_id
+HAVING SUM(
+    CASE
+        WHEN o.com_id = (SELECT com_id FROM company c WHERE c.name='RED') THEN 1
+        ELSE 0
+    END
+) = 0
