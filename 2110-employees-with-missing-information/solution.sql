440cb5a3c6280beca80ce65d7245adc3231cbeb9
Kimba SABI N'GOYE <60729256+kimbasabingoye@users.noreply.github.com>
2024-07-25 15:50:00 +0000

    Sync LeetCode submission Runtime - 583 ms (30.95%), Memory - 0B (100.00%)

--- 2110-employees-with-missing-information/solution.sql
+++ 2110-employees-with-missing-information/solution.sql
@@ -1,7 +1,11 @@
--- Write your PostgreSQL query statement below
+# Write your MySQL query statement below
 WITH T AS
-(SELECT employee_id FROM employees WHERE employee_id NOT IN (SELECT employee_id FROM salaries)
-UNION ALL
-SELECT employee_id FROM salaries WHERE employee_id NOT IN (SELECT employee_id FROM employees))
-SELECT * FROM T ORDER BY employee_id
-
+(SELECT employee_id
+FROM employees e
+WHERE employee_id NOT in (SELECT employee_id FROM salaries)),
+S AS
+(SELECT employee_id FROM salaries s
+WHERE employee_id NOT IN (SELECT employee_id FROM employees)),
+W AS (
+    SELECT * FROM T  UNION ALL SELECT * FROM S)
+SELECT * FROM W order by employee_id
