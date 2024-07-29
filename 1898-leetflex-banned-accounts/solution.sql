76b5b8b780160bec870c4aa18ed77d401a1b19a7
Kimba SABI N'GOYE <60729256+kimbasabingoye@users.noreply.github.com>
2024-07-29 12:55:51 +0000

    Sync LeetCode submission Runtime - 889 ms (7.18%), Memory - 0B (100.00%)

---
+++ 1898-leetflex-banned-accounts/solution.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT DISTINCT l1.account_id 
+FROM loginfo l1 JOIN loginfo l2 ON
+l1.account_id = l2.account_id AND l1.ip_address <> l2.ip_address
+AND (l1.login <= l2.login AND l2.login <= l1.logout) 
