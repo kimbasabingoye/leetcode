5cb904cbdfaec0bf9b0b24652f2614c1716d5624
Kimba SABI N'GOYE <60729256+kimbasabingoye@users.noreply.github.com>
2024-08-18 19:57:44 +0000

    Sync LeetCode submission - Backspace String Compare - Runtime - 36 ms (51.09%), Memory - 16.5 MB (35.80%)

---
+++ 0874-backspace-string-compare/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        stack1 = []
+        stack2 = []
+        
+        for c in s:
+            if c != '#':
+                stack1.append(c) 
+            elif stack1:
+                stack1.pop()
+
+        for c in t:
+            if c != '#':
+                stack2.append(c)
+            elif stack2:
+                stack2.pop()
+        
+        return "".join(stack1) == "".join(stack2) 
